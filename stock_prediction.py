88524b6b0ca2b87d7a5a347482c90ec2255bc3bd
vbxll <walker.414151@gmail.com>
2024-09-21 17:01:00 +0000

    Added stock prediction script, model, and plot for AAPL stock

---
+++ stock_prediction.py
@@ -0,0 +1,10 @@
+
+import yfinance as yf
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Your complete Python code here
+# ...
+    
